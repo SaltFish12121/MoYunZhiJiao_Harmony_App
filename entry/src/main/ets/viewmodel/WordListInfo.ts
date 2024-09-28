85bef7953dd53d7c82edf7f9d5e7a4f992258ab6
saltfish <2646806752@qq.com>
2024-09-28 12:42:53 +0800

    upload 1

--- entry/src/main/ets/viewmodel/WordListInfo.ts
+++ entry/src/main/ets/viewmodel/WordListInfo.ts
@@ -1,9 +1,11 @@
 export default class WordListInfo{
   id: number    //所练习字的ID
   template: any //模板字
+  name: string
 
-  constructor(id: number, template: any) {
+  constructor(id: number, template: any, name: string) {
     this.id = id
     this.template = template
+    this.name = name
   }
 }
--- entry/src/main/ets/viewmodel/analysis/ExerciseAnalysisInfo.ts
+++ entry/src/main/ets/viewmodel/analysis/ExerciseAnalysisInfo.ts
@@ -1,14 +1,12 @@
 export default class ExerciseAnalysisInfo{
   id: number    //用户ID
   frequency: number[] //练习次数
-  schoolScore: number[] //学习练习得分
-  selfScore: number[] //自我练习得分
+  score: number[] //练习得分
 
-  constructor(id: number, frequency: number[], schoolScore: number[], selfScore: number[]) {
+  constructor(id: number, frequency: number[], score: number[]) {
     this.id = id
     this.frequency = frequency
-    this.schoolScore = schoolScore
-    this.selfScore = selfScore
+    this.score = score
   }
 
 }
